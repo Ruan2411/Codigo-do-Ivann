a8fdf088c85cc6f2a68cfa334cb0c6f4bf37a4ab
Ruan Vitor <163296682+Ruan2411@users.noreply.github.com>
2025-03-24 09:30:12 -0300

    Update Hamster.java

--- CadastroDeAnimais/src/br/com/petshop/Hamster.java
+++ CadastroDeAnimais/src/br/com/petshop/Hamster.java
@@ -1,24 +1,21 @@
 package br.com.petshop;
 
-public class Passaro extends Animal {
-	private String raca;
+public class Hamster extends Animal {
+    private String raca;
 
-	public Passaro(String nome, int idade, String raca) {
-		super(nome, idade);
-		this.raca = raca;
+    public Hamster(String nome, int idade, String raca) {
+        super(nome, idade);
+        this.raca = raca;
+    }
 
-	}
+    public void correr() {
+        System.out.println(getNome() + " está correndo na roda!");
+    }
 
-	public void cantar() {
-		System.out.println(getNome() + " está cantarolando!");
-	}
-
-	public void exibirInfo() {
-		System.out.println("\nPássaro");
-		super.exibirInfo();
-		System.out.println("Raça: " + raca);
-		cantar();
-
-	}
-
+    public void exibirInfo() {
+        System.out.println("\nHamster");
+        super.exibirInfo();
+        System.out.println("Raça: " + raca);
+        correr();
+    }
 }
